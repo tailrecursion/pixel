61430a99f3e1e6620b33fb016e68827b456695e2
Alan Dipert <alan@dipert.org>
2013-02-26 14:51:58 -0500

    init

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject thefreshdiet/pixel "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.datomic/datomic-pro "0.8.3826"
+                  :exclusions [postgresql]]])
