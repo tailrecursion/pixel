80120f33d334c4024bd83439ed817f0ae0e9f06a
Alan Dipert <alan@dipert.org>
2013-04-02 01:00:05 -0400

    prepping pixel for prod
    - bumped datomic version
    - brought in tools.logging
    - lein ring for .war file creation

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject thefreshdiet/pixel "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.datomic/datomic-pro "0.8.3826" :exclusions [postgresql]]
+(defproject thefreshdiet/pixel "0.0.1"
+  :description "TFD usage tracking server."
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-pro "0.8.3862"]
                  [ring/ring-core "1.1.6"]
                  [ring/ring-jetty-adapter "1.1.6"]
                  [compojure "1.1.3"]
-                 [ring-edn "0.1.0"]])
+                 [ring-edn "0.1.0"]
+                 [org.clojure/tools.logging "0.2.6"]]
+  :plugins      [[lein-ring "0.8.3"]]
+  :ring {:handler thefreshdiet.pixel/app})
