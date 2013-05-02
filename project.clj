f59cff9e35d15706bfe05ced497def61a82993e7
Alan Dipert <alan@dipert.org>
2013-05-02 16:24:54 -0400

    configuration, deployment improvements
    - using slf4j + clj tools logging
    - logs to console in :dev, to log/ENV.log elsewhere
    - envs/<env>/config.edn for application configuration
    - run with lein with-profile <env> trampoline run

--- project.clj
+++ project.clj
@@ -1,15 +1,35 @@
-(defproject thefreshdiet/pixel "0.0.1"
+(defproject thefreshdiet/pixel "1.0.0-SNAPSHOT"
   :description "TFD usage tracking server."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.8.3862"]
                  [ring/ring-core "1.1.6"]
                  [ring/ring-jetty-adapter "1.1.6"]
                  [compojure "1.1.3"]
+                 [org.clojure/data.json "0.2.2"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/data.json "0.2.2"]]
-  :plugins      [[lein-ring "0.8.3"]]
-  :ring {:handler thefreshdiet.pixel/app}
+                 [log4j "1.2.15"
+                  :exclusions [javax.mail/mail
+                               javax.jms/jms
+                               com.sun.jdmk/jmxtools
+                               com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]
+                 [org.clojure/tools.nrepl "0.2.2"]]
   :main thefreshdiet.pixel
-  :jvm-opts ["-Ddatomic.memcacheServers=jenkins.thefreshdiet.trmk:11211"]
-  :repl-options {:host "jenkins.thefreshdiet.trmk"
-                 :port 4005})
+  :profiles {:dev
+             {:resource-paths ["envs/dev"]
+              :dependencies [[tailrecursion/monocopy "1.0.9"
+                              :exclusions [org.clojure/clojure
+                                           org.slf4j/slf4j-nop
+                                           org.slf4j/log4j-over-slf4j]]]
+              :plugins      [[lein-ring "0.8.3"]]
+              :ring {:handler thefreshdiet.pixel/app}}
+             :production-shared
+             {:dependencies [[com.datomic/datomic-pro "0.8.3862"
+                              :exclusions [org.clojure/clojure
+                                           org.slf4j/slf4j-nop
+                                           org.slf4j/log4j-over-slf4j]]
+                             [tailrecursion/monocopy "1.0.9"
+                              :exclusions [com.datomic/datomic-free]]]}
+             :jenkins.thefreshdiet.trmk-3000
+             [:production-shared
+              {:resource-paths ["envs/jenkins.thefreshdiet.trmk-3000"]
+               :jvm-opts ["-Ddatomic.memcacheServers=jenkins.thefreshdiet.trmk:11211"]}]})
